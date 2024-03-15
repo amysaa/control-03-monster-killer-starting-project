3d71b325463a3234a81d96604bd75eaa49ea3d88
Amisha Verma <92872778+amysaa@users.noreply.github.com>
2024-03-15 20:07:46 +0530

    first commit

---
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -0,0 +1,26 @@
+const ATTACK_VALUE =10;
+const MONSTER_ATTACK_VALUE =14;
+
+let chosenMaxLife=5;
+let currentMonsterHealth=chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+
+adjustHealthBars(chosenMaxLife);
+
+function attackHandler(){
+    const damage = dealMonsterDamage(ATTACK_VALUE);
+     currentMonsterHealth -= damage;
+const playerDamage= dealPlayerDamage(MONSTER_ATTACK_VALUE);
+currentPlayerHealth-=playerDamage;
+if(currentMonsterHealth<=0  && currentPlayerHealth>0 ){
+    alert('you won');
+}else if(currentPlayerHealth<=0 && currentMonsterHealth>0){
+    alert('you lost');
+}else if(currentPlayerHealth<=0 && currentMonsterHealth>0){
+    alert('you have a draw!');
+}
+}
+
+
+attackBtn.addEventListener('click', attackHandler);
+ 
